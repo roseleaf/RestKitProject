621a2715a9d37bf99bc03b8ee9c7a045dfc3fa75
roseleaf <pteroline@gmail.com>
2012-08-14 17:30:53 -0700

    Initial Commit

---
+++ RestKitProjects/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RestKitProjects
+//
+//  Created by Rose CW on 8/14/12.
+//  Copyright (c) 2012 Rose CW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ RestKitProjects/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RestKitProjects
+//
+//  Created by Rose CW on 8/14/12.
+//  Copyright (c) 2012 Rose CW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
